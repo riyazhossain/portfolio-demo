649b65508233aecb28c2cfbc812bd55d1b15f3aa
riyazhossain <riyazhossainmolla8@gmail.com>
2024-09-02 15:16:59 +0530

    Add files via upload

---
+++ pro3.js
@@ -0,0 +1,63 @@
+/* Typing Text Code */
+
+var sliderCounter = 0;
+var sliderContent = [
+    "Developer",
+    "Designer",
+    "coder",
+];
+
+
+var sliderValue = document.querySelector("#SliderValue");
+
+function slide() {
+    if (sliderCounter >= sliderContent.length) {
+        sliderCounter = 0;
+    }
+
+    sliderValue.innerHTML = "";
+    sliderValue.classList.remove("holder-animation")
+    void sliderValue.offsetwidth;
+    sliderValue.classList.add("holder-animation")
+
+    for (i = 0; i < sliderContent[sliderCounter].length; i++) {
+        let letterDiv = document.createElement("div");
+        letterDiv.innerHTML = sliderContent[sliderCounter][i];
+
+
+        if(letterDiv.innerHTML == "") {
+            letterDiv.innerHTML = "&nbsp;";
+            letterDiv.classList.add("start")
+            letterDiv.classList.add("animation")
+            letterDiv.style.animationDelay = i / 10 + "s";
+            sliderValue.appendChild(letterDiv);
+        }
+    }
+    sliderCounter++;
+
+}
+
+slide();
+setInterval(slide, 2000);
+
+/*----Toggle Menu-----*/
+
+$('.menu-btn').click(function(){
+    $('.menu') .toggleClass("active");
+    $('.menu-btn i') .toggleClass('active');
+});
+
+/*---Scroll animation----*/
+
+const sr = ScrollReveal({
+    origin: "top",
+    disance: "80px",
+    duration: 2000,
+    reset: true,
+}) ;
+
+sr.reveal(".featured-text" ,{});
+sr.reveal(".home-img", { delay: 200 });
+
+sr.reveal(".heading", {});
+
